5ee269544a402423cc285ec241bcb174b4cc971c
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-09-24 21:38:10 +0300

    Update ft_rev_bin_num.py

--- ft_rev_bin_num.py
+++ ft_rev_bin_num.py
@@ -1,17 +1,11 @@
-def ft_pow(number, stepen):
-    result = 1
-    stepen2 = 0
-    while stepen2 < stepen:
-        stepen2 += 1
-        result *= number
-    return result
+import ft_pow as pw 
+import ft_len_num as num
 
 
-def ft_rev_bin_num(number):
-    dec = 0
-    stepen = -1
-    while number > 0:
-        stepen += 1
-        dec = dec + (number % 10) * ft_pow(2, stepen)
-        number //= 10
-    return dec
+def ft_rev_bin_num(a):
+    bc = f.ft_len_num(a)
+    b = 0
+    for i in range(bc):
+        b += a % 10 *pw.ft_pow(2, i)
+        a //= 10
+    return b
