969d2cc8dc1135cd0282d685bb6119d42c989cf2
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-09-24 20:00:44 +0300

    Update ft_second_simple_max_num.py

--- ft_second_simple_max_num.py
+++ ft_second_simple_max_num.py
@@ -1,16 +1,56 @@
-def ft_second_simple_max_num(a):
-    d = 0
+def ft_len_num(a):
     b = 0
-    c = a
-    while c > 0:
-        if d < c % 10:
-            d = c % 10
-        c //= 10
+    if a > 0:
+        while a >= 1:
+            b = b + 1
+            a = a / 10
+    if a == 0:
+        b = 0
+    if a < 0:
+        while a <= -1:
+            b = b + 1
+            a = a / 10
+    return b
 
-    while a > 0:
-        if d >= b < a % 10:
-            b = a % 10
+
+def ft_max_num(a):
+    k = ft_len_num(a)
+    m = 0
+    c = 0
+    if a > 0:
+        while k > 0:
+            k = k - 1
+            c = (a % 10)
+            a = a // 10
+            if c > m:
+                m = c
+    elif a == 0:
+        c = 0
+    elif a < 0:
+        a = -a
+        while k > 0:
+            k = k - 1
+            c = (a % 10)
+            a = a // 10
+            if c > m:
+                m = c
+    return m
+
+
+def ft_second_simple_max_num(a):
+    n = ft_len_num(a)
+    k = 0
+    m = ft_max_num(a)
+    if a < 0:
+        a = -a
+        for i in range(n):
+            if a % 10 == m:
+                a //= 10
+            continue
+    if k < a % 10:
+        k = a % 10
         a //= 10
-
-    if b == d:
+    if m == k:
         return -1
+    else:
+        return k
