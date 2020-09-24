afd13a55dc3c16f541ee73c2e9df7591b79afc93
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-09-24 19:14:39 +0300

    Update ft_sum_num.py

--- ft_sum_num.py
+++ ft_sum_num.py
@@ -1,7 +1,34 @@
-def ft_sum_num(a):
+def ft_len_num(a):
     b = 0
-    while a > 0:
-        b += (a % 10)
-        a //= 10
+    if a > 0:
+        while a >= 1:
+            b = b + 1
+            a = a / 10
+    if a == 0:
+        b = 0
+    if a < 0:
+        while a <= -1:
+            b = b + 1
+            a = a / 10
     return b
+
+
+def ft_sum_num(a):
+    k = ft_len_num(a)
+    c = 0
+    if a > 0:
+        while k > 0:
+            k = k - 1
+            c = c + (a % 10)
+            a = a // 10
+    elif a == 0:
+        c = 0
+    elif a < 0:
+        a = -a
+        while k > 0:
+            k = k - 1
+            c = c + (a % 10)
+            a = a // 10
+    return c
+
 
