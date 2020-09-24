b9f658de016cda057e4281ed1aa898a86159a3bf
Reims796 <71494978+Reims796@users.noreply.github.com>
2020-09-24 12:42:37 +0300

    Update ft_min_num.py

--- ft_min_num.py
+++ ft_min_num.py
@@ -1,7 +1,37 @@
-def ft_min_num(a):
-    b = (a % 10)
-    while a > 0:
-        if b < (a % 10):
-            b = (a % 10)
-        a //= 10
+def ft_len_num(a):
+    b = 0
+    if a > 0:
+        while a >= 1:
+            b = b + 1
+            a = a / 10
+    if a == 0:
+        b = 0
+    if a < 0:
+        while a <= -1:
+            b = b + 1
+            a = a / 10
     return b
+
+
+def ft_min_num(a):
+    k = ft_len_num(a)
+    m = 10
+    c = 0
+    if a > 0:
+        while k > 0:
+            k = k - 1
+            c = (a % 10)
+            a = a // 10
+            if c < m:
+                m = c
+    elif a == 0:
+        c = 0
+    elif a < 0:
+        a = -a
+        while k > 0:
+            k = k - 1
+            c = (a % 10)
+            a = a // 10
+            if c < m:
+                m = c
+    return m
